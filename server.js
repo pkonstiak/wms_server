e8d2299dab1ff30c1bf243405ed8780891f9d8bd
Peter <pkonstiak@gmail.com>
2018-10-25 09:19:11 +0200

    update

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-var i=1;
+var express = require("express");
+var server = express();
+var path=require('path');
+var PORT=3000;
+
+server.get("/getCababilities", function(request, response) {
+    console.log(request.query);
+    response.send(request.query);
+  });
+
+
+
+server.listen(PORT, function() {
+    console.log("Server listening on port " + PORT + "!");
+  });
