ca090870195b90ac46b03eb50783dbd220395904
Peter <pkonstiak@gmail.com>
2018-10-25 10:30:45 +0200

    'push'

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var server = express();
 var path=require('path');
-var PORT=3000;
+var PORT=3003;
 
-server.get("/getCababilities", function(request, response) {
-    console.log(request.query);
-    response.send(request.query);
+
+
+
+server.get("/wms", function(request, response) {
+  var params=request.query;
+    console.log(params);
+
+    if(params.service=== 'wms' && params.request === 'GetCapabilities' ){
+      response.sendFile(path.join(__dirname, 'XML_cesty_budovy.xml'))
+
+    }
+
+    else if (params.service=== 'wms' && params.request === 'GetCapabilities' ){            
+      console.log('idem robit get capa')
+    } else {
+      response.send('nejdem robit get capa')
+    }
+    
+  
   });
 
 
