f404cad250ea9dbdad15c326ed1abfd590e9e569
Peter <pkonstiak@gmail.com>
2018-11-14 16:42:19 +0100

    'getMap'

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var server = express();
 var path=require('path');
+var fs = require("fs");
+var mapnik = require("mapnik");
+var generateImage = require('./GetMap.js');
+
+console.log(generateImage);
+
 var PORT=3003;
 
 
@@ -13,6 +19,8 @@ server.get("/wms", function(request, response) {
    if (params.SERVICE=== 'WMS' && params.REQUEST === 'GetCapabilities' ){            
       console.log('idem robit get capa')
       response.sendFile(path.join(__dirname, 'XML_cesty_budovy.xml'))
+	 }else if (params.SERVICE==="WMS"&& params.REQUEST==="GetMap"){
+     generateImage(params, response.sendFile.bind(response))	  
     } else {
       response.send('nejdem robit get capa')
     }
@@ -25,3 +33,5 @@ server.get("/wms", function(request, response) {
 server.listen(PORT, function() {
     console.log("Server listening on port " + PORT + "!");
   });
+  
+ 
